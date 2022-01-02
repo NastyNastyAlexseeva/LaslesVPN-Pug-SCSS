02f21f134d132ca43b98ed6717d915d0fdec8d07
DmitriyZ <toropagka@gmail.com>
2022-01-02 23:08:20 +0300

    dayz slider done, some change other files

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
     // alert(1);
     $(".slider-slick").slick({
         dots: true,
+        slidesToShow: 3,
+        speed: 500,
+        easing: "linear",
+        variableWidth: true,
+
     });
 });
 
