db959bab94c212a6d77571eaf01cfaa5c51edc9c
DmitriyZ <toropagka@gmail.com>
2022-01-06 17:22:11 +0300

    dayz footer in process

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(".slider-slick").slick({
         dots: true,
         slidesToShow: 3,
-        speed: 500,
+        speed: 600,
         easing: "linear",
         variableWidth: true,
 
