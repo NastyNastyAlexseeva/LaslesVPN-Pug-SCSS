43a827e9e88ba488bf3ec3998575f0a3fb1e5003
DmitriyZ <toropagka@gmail.com>
2022-01-12 23:23:01 +0300

    dayz adaptive footer done

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   
     $(".slider-slick").slick({
         dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 2,
+        slidesToShow: 2, //хз, как выводить только 3 точки. Увеличить слайдскролл - не вариант
+        slidesToScroll: 1,
         speed: 600,
         easing: "linear",
         variableWidth: true,
@@ -11,3 +11,63 @@ $(document).ready(function () {
     });
 });
 
+
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+
+// PRODUCT
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.footer-title_nav')) {
+      var dropdowns = document.getElementsByClassName("dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  //ENGAGE
+  function myFunctions() {
+    document.getElementById("myDropdowns").classList.toggle("show");
+  }
+  
+ 
+  window.onclick = function(event) {
+    if (!event.target.matches('.footer-title_nav')) {
+      var dropdowns = document.getElementsByClassName("dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  //EARN MONEY
+  function myFunctionz() {
+    document.getElementById("myDropdownz").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.footer-title_nav')) {
+      var dropdowns = document.getElementsByClassName("dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+ 
